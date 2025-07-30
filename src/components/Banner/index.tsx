d0f5b25c89ebab1aa57df9b9f6ad5df8773b7028
arnor <arnorsantosnunes01@gmail.com>
2025-07-29 21:23:28 -0300

    atualizacao

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,7 +7,7 @@ const Banner = () => (
       <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
       <Precos>
         De <span>R$ 250,00</span> <br />
-        por apenas R$ 99,99
+        por apenas R$ 99,90
       </Precos>
     </div>
   </Imagem>
